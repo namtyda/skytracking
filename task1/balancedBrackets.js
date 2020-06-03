2af18f029c6eaf2e8037fcf9581fb38141ab91fd
Andrey Pogorelov <namtydapro@gmail.com>
2020-06-03 11:30:32 +0300

    balanced brackets

---
+++ task1/balancedBrackets.js
@@ -0,0 +1,25 @@
+/**
+ * Проверка на сбалансированность фигурных скобкок
+ * @param  {string}  input
+ * @return {boolean}
+ */
+function isBalanced(input) {
+  const pattern = {
+    '}': '{'
+  }
+  const stack = [];
+
+  for (const char of input) {
+    if (/[\{]/.test(char)) {
+      stack.push(char);
+    } else {
+      const open = stack.pop();
+      if (!open) return false;
+      if (pattern[char] !== open) return false;
+    }
+  }
+  return stack.length === 0;
+}
+
+console.log('balanced:', isBalanced('{{}{}}{}')); // true
+console.log('not balanced:', isBalanced('{}{{}')); // false
